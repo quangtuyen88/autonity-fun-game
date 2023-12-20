71fdd8e06a201c4ffa5f72fd2be1e284163643a5
tquang@mysuki.io <tquang@mysuki.io>
2023-12-20 23:12:22 +0800

    init repo

---
+++ send_txt.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+get_current_nonce() {
+    docker exec -i aut_client /root/.local/bin/aut account info | grep '"tx_count"' | awk '{print $2}' | tr -d ','
+}
+
+# Function to execute a single transaction
+## change 5 to ATN value you wanna send each txt
+perform_transaction_to_wallet1() {
+          local specific_nonce=$1
+    docker exec -i aut_client /root/.local/bin/aut tx make --to first_wallet --value 5 -n $specific_nonce |
+    docker exec -i aut_client /root/.local/bin/aut tx sign --password your_auth_pass - |
+    docker exec -i aut_client /root/.local/bin/aut tx send -
+}
+
+perform_transaction_to_wallet2() {
+    local specific_nonce=$1
+    docker exec -i aut_client /root/.local/bin/aut tx make --to second_wallet --value 5 -n $specific_nonce |
+    docker exec -i aut_client /root/.local/bin/aut tx sign --password your_auth_pass - |
+    docker exec -i aut_client /root/.local/bin/aut tx send -
+}
+
+nonce=$(get_current_nonce)
+# Infinite loop to send a transaction every second
+while true; do
+    perform_transaction_to_wallet1 $((nonce++))
+    perform_transaction_to_wallet2 $((nonce++))
+done
